51be209c6d84ea26d80634a4d9a0b21bcfcb5a61
Jack Hall-Tipping <jack.halltipping@gmail.com>
2015-06-15 11:47:56 +0100

    create board method with dimensions, and create ship class

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,7 @@
+require_relative 'ship'
+
 class Board
-  
+  def dimensions num
+    board = Array.new(num){Array.new(num)}
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,4 +2,12 @@ require 'board'
 
 describe Board do
 
+  it { is_expected.to respond_to(:dimensions).with(1).argument }
+
+  describe 'dimensions' do
+    it 'creates a board with the given dimensions' do
+      expect(subject.dimensions 2).to eq [[nil, nil], [nil, nil]]
+    end
+  end
+
 end
