f48ae2798ea4b814c62ba9a995b162cb944a2f5c
Jonathan Ng <jonngkh@hotmail.com>
2015-06-16 16:43:33 +0100

    doubles

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -17,33 +17,37 @@ describe Board do
   describe 'placing' do
 
     it 'stores the ship in the fleet' do
-      ship = Ship.new
+      ship = double :ship
+      allow(ship).to receive(:position=)
       expect { subject.place(ship, 'A1') }.to change { subject.fleet.length }.from(0).to(1)
     end
 
     it 'sets the position of the ship as it is being placed' do
-      #ship = double :ship, :position => ''
-      ship = Ship.new
-      expect { subject.place(ship, "A1") }.to change { ship.position }.from([]).to(['A1'])
+      ship = double :ship, :position => []
+      #ship = Ship.new
+      expect(ship).to receive(:position=).with(["A1"])
+      subject.place(ship, 'A1')
     end
 
     it 'sets the position of the ship even with multiple positions' do
-      ship = Ship.new
-      expect { subject.place(ship, "A1, A2") }.to change { ship.position }.from([]).to(['A1', 'A2'])
+      ship = double :ship
+      allow(ship).to receive(:position)
+      expect(ship).to receive(:position=).with(["A1", "A2"])
+      subject.place(ship, "A1, A2")
     end
   end
 
   describe 'firing' do
 
     it 'registers a hit when called' do
-      ship = Ship.new
+      ship = double :ship
+      allow(ship).to receive(:position=).with( ['A1'] )
       subject.place(ship, 'A1')
+      allow(ship).to receive(:position).and_return(['A1'])
       expect(subject.fire('A1')).to eq('Hit!')
     end
 
     it 'registers a miss when called but no boat in location' do
-      ship = Ship.new
-      subject.place(ship, 'A2')
       expect(subject.fire('A1')).to eq('Miss!')
     end
   end
